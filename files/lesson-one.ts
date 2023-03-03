72c54854801766131fdd83bf97e18a850c2b51b2
TCotton <me@andywalpole.me>
2023-03-03 06:44:48 +0000

    more work with types

--- files/lesson-one.ts
+++ files/lesson-one.ts
@@ -6,7 +6,7 @@ interface Fruit extends EdibleThing {
     color: string;
 }
 
-const apple: { color: string; name: string } = { name: "Apple", color: "red" };
+const apple: Fruit = { name: "Apple", color: "red" };
 
 const foodBasket: EdibleThing[] = [];
 foodBasket.push(apple);
